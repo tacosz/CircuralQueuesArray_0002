8d40bc32346ec36880edddfae0baf4d618314599
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-30 18:35:53 +0700

    fungsi else digunakan untuk cek front position lebih besar dari rear position maka iterasi dari front hingga arkhir array

--- CircuralQueuesArray/CircuralQueuesArray.cpp
+++ CircuralQueuesArray/CircuralQueuesArray.cpp
@@ -94,6 +94,25 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
 
