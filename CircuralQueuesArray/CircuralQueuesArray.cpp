23d6078d70c779d12ef4087fafb579c737392da4
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-30 18:49:35 +0700

    switch case 3, 4, dan default

--- CircuralQueuesArray/CircuralQueuesArray.cpp
+++ CircuralQueuesArray/CircuralQueuesArray.cpp
@@ -146,7 +146,26 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
             }
         }
+        catch (exception& e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
     }
+    return 0;
 }
