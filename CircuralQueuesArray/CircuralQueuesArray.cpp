b222c81e0c33ca1f1714de8026d13d8d59279bb6
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-30 18:30:21 +0700

    fungsi if menguji front position lebih kecil sama dengan rear position

--- CircuralQueuesArray/CircuralQueuesArray.cpp
+++ CircuralQueuesArray/CircuralQueuesArray.cpp
@@ -81,6 +81,19 @@ public:
             cout << "Queue is empty\n";
             return;
         }
+        
+        cout << "\nElements in the queue are...\n";
+
+        //jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
 
