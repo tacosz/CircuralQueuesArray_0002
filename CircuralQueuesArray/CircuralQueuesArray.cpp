b5ff36ded5493730261dca46280fdc3f639f8685
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-30 18:40:55 +0700

    menampilkan menu pilihan

--- CircuralQueuesArray/CircuralQueuesArray.cpp
+++ CircuralQueuesArray/CircuralQueuesArray.cpp
@@ -120,4 +120,19 @@ int main()
 {
     Queues q;
     char ch;
+
+    while (true)
+    {
+        try 
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+        }
+    }
 }
